beac307488b6585158a97e1e9ced6a00585961c9
d911183 <Warren.Shipp@team.telstra.com>
2020-09-02 12:44:34 +1000

    Setting up react front end

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "warrens-notes-app"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://r8q7tfjaw1.execute-api.us-east-2.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_r9nUQC3VG",
+    APP_CLIENT_ID: "4ov6ofvjtvmaol3p9mdkjtb8tj",
+    IDENTITY_POOL_ID: "us-east-2:6e91df3e-dc08-4c05-ae0f-4c4bd1b0e271"
+  }
+};
